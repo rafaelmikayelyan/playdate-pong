15a8a514b1d17a72d259bf09b91eda2a85efb205
Rafael Mikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2022-05-20 11:26:14 -0700

    feat(player): add crank controls

--- source/player.lua
+++ source/player.lua
@@ -5,7 +5,7 @@ local GFX <const> = PD.graphics
 
 class('Player').extends(Paddle)
 
-local TOP <const> = 8
+local TOP <const> = 12
 local BOTTOM <const> = 228
 
 function Player:init(x,y)
@@ -13,6 +13,7 @@ function Player:init(x,y)
 end
 
 function Player:update()
+	local crankChange, crankAccelerated = PD.getCrankChange()
 	-- Player.super.update(self)
 	if PD.buttonIsPressed(PD.kButtonUp) then
 		if self.y > TOP then
@@ -21,6 +22,15 @@ function Player:update()
 	elseif PD.buttonIsPressed(PD.kButtonDown) then
 		if self.y < BOTTOM then
 			self:moveBy(0, self.speed)
+		end
+	end
+	if crankChange > 0 then
+		if self.y < BOTTOM then
+			self:moveBy(0, crankChange)
+		end
+	elseif crankChange < 0 then
+		if self.y > TOP then
+			self:moveBy(0, crankChange)
 		end
 	end
 end
