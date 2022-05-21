feab6c5b91a476a6c51ec69814cc75bc98c22718
Rafael Mikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2022-05-21 12:47:34 -0700

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -d build/ ]
-then
-mkdir build/ && echo "-> created build/"
-fi
-
-readline=$(head -n 1 source/pdxinfo)
-name=${readline#*=}
-
-pdc source build/${name}.pdx && open build/${name}.pdx
