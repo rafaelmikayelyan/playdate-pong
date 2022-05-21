2064a0b3b3dd21375510a5892e39349a56746832
Rafael Mikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2022-05-20 21:22:47 -0700

    feat(ball): add angle calculation

--- source/ball.lua
+++ source/ball.lua
@@ -4,6 +4,10 @@ local GFX <const> = PD.graphics
 class('Ball').extends(GFX.sprite)
 
 local r <const> = 4
+local screenCenter <const> = {x = PD.display.getWidth() / 2, y = PD.display.getHeight() / 2}
+local speed <const> = 8
+
+local direction
 
 function Ball:init(x, y)
 	-- not necessary
@@ -12,6 +16,7 @@ function Ball:init(x, y)
 	self.x = x
 	self.y = y
 	self.r = r
+	self.vector = {x = speed, y = 0}
 
 	self:moveTo(x, y)
 
@@ -24,4 +29,74 @@ function Ball:init(x, y)
 	self:setCollideRect(0, 0, self:getSize())
 	
 	self:add()
+end
+
+local function resetBall(self)
+	self:moveTo(screenCenter.x, screenCenter.y)
+	self.vector.x = speed * getTurn().turn
+	self.vector.y = 0
+end
+
+local function ricochet(self, paddle)
+	direction = self.vector.x / math.abs(self.vector.x)
+	local multiAngle = true
+
+	-- calculate the angle
+	if multiAngle then
+		ratio = (self.y - (paddle.y - paddle.r)) / (2 * paddle.r)
+		if ratio < 0.1 then
+			ratio = 0.1
+		elseif ratio > 0.9 then
+			ratio = 0.9
+		end
+
+		angle = 180 * ratio
+		
+		sinB = math.sin(math.rad(angle))
+		cosB = math.cos(math.rad(angle))
+		sinA = math.sin(math.rad(90-angle))
+		self.vector.x = speed * sinB * -direction
+		self.vector.y = speed * sinA * direction
+
+		print('Vxy: '..self.vector.x..' '..self.vector.y..'\t\tangle: '..angle..'\tspeed: '..math.sqrt(self.vector.x^2 + self.vector.y^2)..'\tsinB cosB: '..sinB..' '..cosB)
+
+	-- simple 2-angle
+	else
+		if self.y - self.r <= paddle.y and self.y + self.r >= paddle.y then
+			self.vector.x = speed * -direction
+			self.vector.y = 0
+		else
+			if self.y < paddle.y - paddle.r or self.y > paddle.y + paddle.r then
+				self.vector.x = speed * direction / 2
+			else
+				self.vector.x = speed * -direction / 2
+			end
+			if self.y < paddle.y then
+				self.vector.y = -speed
+			else
+				self.vector.y = speed
+			end
+		end
+	end
+end
+
+function Ball:update()
+	local actualX, actualY, collisions, length = self:moveWithCollisions(self.x + self.vector.x, self.y + self.vector.y)
+	if length > 0 then
+		for index, collision in pairs(collisions) do
+			local collidedObject = collision['other']
+			if collidedObject:isa(Player) or collidedObject:isa(Paddle) then
+				ricochet(self, collidedObject)
+			end
+		end
+	elseif actualX < 0 or actualX > 400 then
+		resetBall(self)
+		addScore(actualX)	
+	elseif actualY < 0 + self.r or actualY > 240 - self.r then
+		self.vector.y = -self.vector.y
+	end
+end
+
+function Ball:collisionResponse()
+    return 'bounce'
 end
