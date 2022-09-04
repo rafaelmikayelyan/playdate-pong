74deb13b45284a5572d184ca993377f340ddef19
Rafael Mikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2022-09-04 16:19:16 -0700

    refactor(build): change head to awk

--- build.sh
+++ build.sh
@@ -3,10 +3,8 @@ then
 mkdir build/ && echo "-> created build/"
 fi
 
-readline=$(head -n 1 source/pdxinfo)
-name=${readline#*=}
-readlineversion=$(head -n 5 source/pdxinfo | tail -n 1)
-version=${readlineversion#*=}
-filename=${name}-${version}.pdx
+name=$(awk '/name/' scriptsource)
+version=$(awk '/version/' scriptsource)
+filename=${name#*=}-${version#*=}.pdx
 
 pdc source build/$filename && open build/$filename
